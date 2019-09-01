6f098a733dd3aea86e42f9534c07bcfb10117431
Sayeed Afridi <as.afridi563@gmail.com>
2019-09-02 03:51:19 +0600

    Updates

---
+++ precache-manifest.a79ce238b53bcda65ac432cc877347bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a71506ef25f59e9c9ef9dd27cb6726",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0644e843b507bb1f9418",
+    "url": "/monster-rolodex/static/css/main.d4e6a5ed.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monster-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "0644e843b507bb1f9418",
+    "url": "/monster-rolodex/static/js/main.da98475a.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
