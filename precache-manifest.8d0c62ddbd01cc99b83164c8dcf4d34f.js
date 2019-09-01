e76a6cf031e7c16d10855978b67634f1291a2c9c
Sayeed Afridi <as.afridi563@gmail.com>
2019-09-02 04:21:28 +0600

    adding files

---
+++ precache-manifest.8d0c62ddbd01cc99b83164c8dcf4d34f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c026dcb12b959dda5ff23845c59f9d6d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0644e843b507bb1f9418",
+    "url": "/monster-rolodex/static/css/main.d4e6a5ed.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monster-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "0644e843b507bb1f9418",
+    "url": "/monster-rolodex/static/js/main.da98475a.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
